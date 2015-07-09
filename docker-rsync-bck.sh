aa586444deba9c8a6951ae7cbe7e0e89906fbd07
Antonio De Marinis <demarinis@eea.europa.eu>
2015-07-09 10:38:27 +0200

    modified docker-rsync-bck.sh to identify docker daemon version

--- docker-rsync-bck.sh
+++ docker-rsync-bck.sh
@@ -1,3 +1,4 @@
+##########docker volume backup final version with docker structure, also using docker version updated by Adrian
 #!/bin/bash
 
 if [[ ! -d "/var/local/dockerbck/volume-copy" ]]; then
@@ -5,12 +6,27 @@ if [[ ! -d "/var/local/dockerbck/volume-copy" ]]; then
 fi
 
 chmod 777 /var/local/dockerbck/volume-copy;
+dockerver=$(docker -v | gawk '{print $3}' | sed 's/,//g')
+
+case $dockerver in
+        1.5.0|1.6.0)
+                voldir="/var/lib/docker/vfs/dir/"
+                ;;
+        *)
+                voldir="/var/lib/docker/volumes/"
+                ;;
+esac
+
+
 cd /var/local/dockerbck;
-ls /var/lib/docker/vfs/dir/ > dockervolumes.txt;
+ls $voldir > dockervolumes.txt;
+
 for i in `cat dockervolumes.txt`; do
-docker run --rm -ti -v /var/lib/docker/vfs/dir/$i:/$i  -v $(pwd)/volume-copy:/backup rsync/centos rsync -avrz /$i /backup
+        docker run --rm -ti -v $voldir$i:/$i  -v $(pwd)/volume-copy:/backup rsync/centos rsync -avrz /$i /backup
 done;
+
 docker ps -a | grep data | gawk '{print $NF}' > containers.txt;
+
 for i in `cat containers.txt`; do
-docker inspect $i | grep "vfs/dir" | awk '/"(.*)"/ { gsub(/"/,"",$2); print $2 }' | sed 's/,//g' > volume-copy/$i-volpath.txt;
+        docker inspect $i | grep $voldir | awk '/"(.*)"/ { gsub(/"/,"",$2); print $2 }' | sed 's/,//g' > volume-copy/$i-volpath.txt;
 done;
